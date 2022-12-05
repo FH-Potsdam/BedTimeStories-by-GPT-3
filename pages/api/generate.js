54c74470ca80cd036bf93f403c202fbeb53cb52a
Rahfl <119874560+Rahfl@users.noreply.github.com>
2022-12-05 17:47:12 +0100

    Add files via upload

---
+++ pages/api/generate.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+var lastResult;
+var printLastResult;
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: generatePrompt(lastResult + req.body.input_prompt),
+    max_tokens: 250,
+    temperature: 0.5,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+
+  lastResult = completion.data.choices[0].text;
+  var sentences = lastResult.split('.');
+sentences.pop();
+printLastResult = sentences.join('.');
+console.log(printLastResult);
+}
+
+function generatePrompt(input_prompt) {
+  const capitalizedinput_prompt =
+    input_prompt[0].toUpperCase() + input_prompt.slice(1).toLowerCase();
+  //console.log(capitalizedinput_prompt);
+  return `continue a short bedtime story. The story begins anxiously, but in the end it spreads a positive attitude. The story starts as follows: ${capitalizedinput_prompt}. At the end of the input there is a question and an answer. First, rephrase these two sentences in a narrative way. Then, continue the story in two sentences. Then ask a question about a possible continuation of the story.`;
+
+//input_prompt: Cat
+//Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+//input_prompt: Dog
+//Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+//input_prompt: ${capitalizedinput_prompt}
+//Names:`;
+}
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [input_prompt, setinput_prompt] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input_prompt: input_prompt }),
+    });
+    const data = await response.json();
+    setResult(data.result);
+    setinput_prompt("");
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/dog.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/dog.png" className={styles.icon} />
+        <h3>Bedtime stories about ...</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="input_prompt"
+            placeholder="Enter a word"
+            value={input_prompt}
+            onChange={(e) => setinput_prompt(e.target.value)}
+          />
+          <input type="submit" value="Generate story" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
